8b95d02e160e9765ad8f7a7b463635e546cb668c
Terrance Corley <terrancecorley@outlook.com>
2018-10-30 10:33:17 -0700

    Updates

---
+++ precache-manifest.a21208668876050677e7f92429a50876.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ricky-and-morty-api-clone/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3720f88df0a570924ede",
+    "url": "/ricky-and-morty-api-clone/static/js/runtime~main.3720f88d.js"
+  },
+  {
+    "revision": "65d4595769750e0d0c94",
+    "url": "/ricky-and-morty-api-clone/static/js/main.65d45957.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/ricky-and-morty-api-clone/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "65d4595769750e0d0c94",
+    "url": "/ricky-and-morty-api-clone/static/css/main.e8973d1d.chunk.css"
+  },
+  {
+    "revision": "0bd340a50fbf7ebaa81771a9c46c45df",
+    "url": "/ricky-and-morty-api-clone/index.html"
+  }
+];

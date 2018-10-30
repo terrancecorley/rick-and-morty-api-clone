0e8db61520282e83cd7a95b5cf5c6c5a38af0d3d
Terrance Corley <terrancecorley@outlook.com>
2018-10-30 12:25:16 -0700

    added custom fonts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import React, { Component, Fragment } from 'react';
 import styled, { createGlobalStyle } from 'styled-components';
+import schwifty from './assets/fonts/schwifty.woff';
+import schwifty2 from './assets/fonts/schwifty.woff2';
+import schwifty3 from './assets/fonts/schwifty.ttf';
 
 const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'schwifty';
+    src: 
+      url(${schwifty2}) format('woff2'),
+      url(${schwifty}) format('woff'),
+      url(${schwifty3}) format('truetype');
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: inherit;
+  }
+
+  :root {
+    box-sizing: border-box;
+    font-size: 1rem;
+
+    --color--primary: ;
+    --color--secondary ;
+    --bg-color--primary ;
+    --bg-color--secondary ;
+    --font-family--primary 'Open Sans', sans-serif;
+    --font-family--secondary: 'schwifty', 'Open Sans', sans-serif;
+  }
+
   body {
     line-height: 1.5;
   }
